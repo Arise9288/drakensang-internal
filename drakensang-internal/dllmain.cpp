0c80bd0e77e08dcfd8523cc1f4035984c580bdf3
Arise9288 <Arise9288@proton.me>
2024-05-10 21:16:18 -0700

    creating main thread on inject + writing console

--- drakensang-internal/dllmain.cpp
+++ drakensang-internal/dllmain.cpp
@@ -1,22 +1,37 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <iostream>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
+HINSTANCE dllHandle;
+uintptr_t moduleBase;
+
+DWORD WINAPI EjectThread(LPVOID lpParameter) {
+    FreeLibraryAndExitThread(dllHandle, 0);
+    return 0;
+}
+
+DWORD WINAPI MainThread() {
+    moduleBase = (uintptr_t)GetModuleHandle(L"dro_client64.exe");
+    AllocConsole();
+    FILE* fp;
+    freopen_s(&fp, "CONOUT$", "w", stdout);
+
+
+    printf("Module base: %llx\n", moduleBase);
+
+
+    //fclose(fp);
+    FreeConsole();
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)EjectThread, NULL, 0, NULL);
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ulReasonForCall, LPVOID lpReserved) {
+    if (ulReasonForCall == DLL_PROCESS_ATTACH) {
+        dllHandle = hModule;
+        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)MainThread, NULL, 0, NULL);
     }
     return TRUE;
 }
-
